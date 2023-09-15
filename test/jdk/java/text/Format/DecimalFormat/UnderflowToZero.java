f1a6bf716511ae9da96cf6b0da63dbeb726d0c68
Justin Lu <justin.lu@oracle.com>
2023-09-15 00:37:52 -0700

    Fix copyright header

--- test/jdk/java/text/Format/DecimalFormat/UnderflowToZero.java
+++ test/jdk/java/text/Format/DecimalFormat/UnderflowToZero.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -26,7 +24,8 @@
 /*
 * @test
 * @bug 8174722
-* @summary Tests underflow for rounding values < abs(0.1) in DecimalFormat
+* @summary Tests underflow to zero for rounding values < abs(0.1) in DecimalFormat
+*          for the different RoundingModes
 * @run junit UnderflowToZero
 */
 
